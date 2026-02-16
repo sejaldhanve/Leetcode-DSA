d461439008554777e0420dc0f3a3387a57186ba7
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-02-16 23:20:18 +0530

    Time: 4 ms (42.29%) | Memory: 43.2 MB (86.33%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='(' || c=='[' || c=='{'){
+                st.push(c);
+            }
+            else{
+                if (st.isEmpty()) {
+                    return false;
+                }
+                char top=st.peek();
+                st.pop();
+                if(c==')' && top=='(' || c==']' && top=='[' || c=='}' && top=='{'){
+                    continue;
+                }
+                else 
+                return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
