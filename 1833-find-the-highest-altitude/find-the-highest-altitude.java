bb646b7cadd28cc809d54ea825c8607f1789c866
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-03-14 23:44:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (93.53%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -3,7 +3,7 @@ class Solution {
         int[] newarr=new int[gain.length+1];
         newarr[0]=0;
         int highest=0;
-        for(int i=1;i<gain.length+1;i++){
+        for(int i=1;i<=gain.length;i++){
             newarr[i]=newarr[i-1]+gain[i-1];
             if(newarr[i]>highest){
                 highest=newarr[i];
