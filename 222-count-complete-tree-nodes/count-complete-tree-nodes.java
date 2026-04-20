86a69f3cf0f00751d22d10b05bfcc98eeb7e862e
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-04-20 20:51:02 +0530

    Time: 8 ms (6.73%) | Memory: 49.9 MB (5.60%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -23,16 +23,15 @@ class Solution {
         q.offer(root);
         while(!q.isEmpty()){
             int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode node=q.poll();
-                count++;
-                if(node.left!=null){
-                    q.offer(node.left);
-                }
-                if(node.right!=null){
-                    q.offer(node.right);
-                }
+            TreeNode node=q.poll();
+            count++;
+            if(node.left!=null){
+                q.offer(node.left);
             }
+            if(node.right!=null){
+                q.offer(node.right);
+            }
+
         }
         return count;
         
