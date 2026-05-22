e9ad13e23a8617e90ac93511df1d550df1048884
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-05-22 15:37:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (32.29%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,14 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String str) {
-        String sub="";
         str=str.trim();
+        int count=0;
         for(int i=str.length()-1;i>=0;i--){
             if(str.charAt(i)==' '){
                 break;
             }
-            sub+=str.charAt(i);
+            count++;
         }
-        return sub.length();
+        return count;
     }
     
 }
