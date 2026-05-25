113a104c73033fd0dd0bd34d157e09b97424c0ac
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-05-25 20:39:11 +0530

    Time: 12 ms (46.09%) | Memory: 46.1 MB (77.78%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -2,7 +2,6 @@ class Solution {
     public int characterReplacement(String s, int k) {
         int l=0;
         int r=0;
-        int len=0;
         int maxFreq=0;
         int ans=0;
         int[] freq=new int[26];
