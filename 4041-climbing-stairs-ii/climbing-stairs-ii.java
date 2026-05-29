9b7c31440aac4faafffec21dce5760be5e1360ef
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-05-29 15:32:41 +0530

    Time: 7 ms (68.89%) | Memory: 117.2 MB (48.87%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -1,26 +1,20 @@
 class Solution {
-    private int func(int index, int n, int[] costs, int[] dp){
-        if(index==n){
-            return 0;
-        }
-        if(dp[index]!=-1){
-            return dp[index];
-        }
-        int ans=Integer.MAX_VALUE;
-        for(int jump=1;jump<=3;jump++){
-            int next=index+jump;
-            if(next<=n){
-                int curCost=costs[next-1]+jump*jump+func(next, n, costs, dp);
-                ans=Math.min(ans, curCost);
-            }
-        }
-        return dp[index]=ans;
-    }
     public int climbStairs(int n, int[] costs) {
         int[] dp=new int[n+1];
-        int index=0;
-        Arrays.fill(dp, -1);
-        return func(index, n, costs, dp);
-        
+        dp[n]=0;
+
+        for(int i=n-1;i>=0;i--){
+            int ans=Integer.MAX_VALUE;
+            for(int jump=1;jump<=3;jump++){
+                int next=i+jump;
+                if(next<=n){
+                    int curCost=costs[next-1]+jump*jump+dp[next];
+                    ans=Math.min(ans, curCost);
+                }
+            }
+            dp[i]=ans;
+        }
+        return dp[0];  
+
     }
 }
