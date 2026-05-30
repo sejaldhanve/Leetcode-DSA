392218d3467977c2f30f8c57f1440e14d7b226ad
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-05-31 00:54:27 +0530

    Time: 52 ms (10.78%) | Memory: 47 MB (92.15%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,27 +1,22 @@
 class Solution {
-    private int func(int index, int[] nums, int[] dp){
-        if(index>=nums.length-1){
-            return 0;
-        }
-        int ans=Integer.MAX_VALUE;
-        if(dp[index]!=-1){
-            return dp[index];
-        }
-        for(int nextJump=1;nextJump<=nums[index];nextJump++){
-            int nextPos=index+nextJump;
-            if(nextPos<nums.length){
-                int sub=func(nextPos, nums, dp);
-                if(sub!=Integer.MAX_VALUE){
-                    ans=Math.min(ans, 1+sub);
+    public int jump(int[] nums) {
+        int[] dp=new int[nums.length+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        int index=0;
+        dp[nums.length-1]=0;
+
+        for(int i=nums.length-2;i>=0;i--){
+            for(int nextJump=1;nextJump<=nums[i];nextJump++){
+                int nextPos=i+nextJump;
+                if(nextPos<nums.length){
+                    int sub=dp[nextPos];
+                    if(sub!=Integer.MAX_VALUE){
+                        dp[i]=Math.min(dp[i], 1+sub);
+                    }
                 }
             }
         }
-        return dp[index]=ans;
-    }
-    public int jump(int[] nums) {
-        int[] dp=new int[nums.length+1];
-        Arrays.fill(dp, -1);
-        int index=0;
-        return func(index, nums, dp);  
+        return dp[0];
+          
     }
 }
