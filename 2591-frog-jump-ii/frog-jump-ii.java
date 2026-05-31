39eb4fd11705f84ce3b2aca59d9c71f1ba975961
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-05-31 15:52:14 +0530

    Time: 28 ms (5.10%) | Memory: 94.3 MB (6.08%) - LeetSync

--- 2591-frog-jump-ii/frog-jump-ii.java
+++ 2591-frog-jump-ii/frog-jump-ii.java
@@ -1,17 +1,22 @@
 class Solution {
-    private int func(int[] stones, int index){
+    private int func(int[] stones, int index, int[] dp){
         if(index>=stones.length){
             return 0;
         }
+        if(dp[index]!=-1){
+            return dp[index];
+        }
         int curJump=stones[index]-stones[index-2];
-        return Math.max(curJump, func(stones, index+1));
+        return dp[index]=Math.max(curJump, func(stones, index+1, dp));
     }
     public int maxJump(int[] stones) {
+        int[] dp =new int[stones.length+1];
+        Arrays.fill(dp, -1);
         if(stones.length==2){
             return stones[1]-stones[0];
         }
 
-        return Math.max(stones[1]-stones[0], func(stones, 2));
+        return Math.max(stones[1]-stones[0], func(stones, 2, dp));
         
     }
 }
