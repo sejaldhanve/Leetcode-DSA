10da6acd41c7247ceace0da7b28fc7519ccc725a
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-07-01 14:51:41 +0530

    Time: 228 ms (18.09%) | Memory: 119.3 MB (69.21%) - LeetSync

---
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
@@ -0,0 +1,84 @@
+class Solution {
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+        int n=grid.size();
+        int[][] dir={{0,1}, {0,-1}, {1,0}, {-1,0}};
+        int[][] dist=new int[n][n];
+        for(int[] row:dist){
+            Arrays.fill(row, -1);
+        }
+        Queue<int[]> q=new LinkedList<>();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid.get(i).get(j)==1){
+                    dist[i][j]=0;
+                    q.offer(new int[]{i,j});
+                }
+            }
+        }
+
+        while(!q.isEmpty()){
+            int[] cur=q.poll();
+            int r=cur[0];
+            int c=cur[1];
+
+            for(int[] d:dir){
+                int nr=r+d[0];
+                int nc=c+d[1];
+
+                if(nr>=0 && nr<n && nc>=0 && nc<n && dist[nr][nc]==-1){
+                    dist[nr][nc]=dist[r][c]+1;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+        }  
+
+        //binary search;
+        int low=0;
+        int high=2*n;
+        int ans=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(canReach(dist, mid)){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }  
+        return ans;
+    }
+    private boolean canReach(int[][] dist, int safe){
+        int n=dist.length;
+        int[][] dir={{0,1}, {0,-1}, {1,0}, {-1,0}};
+        if(dist[0][0]<safe){
+            return false;
+        }
+        boolean[][] vis=new boolean[n][n];
+        Queue<int[]> q=new LinkedList<>();
+        q.offer(new int[]{0,0});
+
+        while(!q.isEmpty()){
+            int[] cur=q.poll();
+            int r=cur[0];
+            int c=cur[1];
+
+            if(r==n-1 && c==n-1){
+                return true;
+            }
+
+            for(int[] d:dir){
+                int nr=r+d[0];
+                int nc=c+d[1];
+
+                if(nr>=0 && nr<n && nc>=0 && nc<n && !vis[nr][nc] && dist[nr][nc]>=safe){
+                    vis[nr][nc]=true;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+        } 
+
+    return false;
+    }
+}
