e6c4e649a5b0a7afc94fa995fd97cb376a16bd6b
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-07-24 15:06:51 +0530

    Time: 3 ms (22.80%) | Memory: 44.3 MB (9.07%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -2,8 +2,7 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         for(int i=m;i<nums1.length;i++){
             nums1[i]=nums2[i-m];
-
-        }
-        Arrays.sort(nums1);        
+        } 
+        Arrays.sort(nums1);
     }
 }
