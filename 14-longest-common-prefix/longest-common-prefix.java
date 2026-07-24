11509017742edfa5ca153e95ef6a0c976a04072d
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-07-25 00:06:14 +0530

    Time: 4 ms (12.39%) | Memory: 43.4 MB (16.87%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,15 +1,17 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs==null || strs.length==0){
-            return "";
-        }
+        String str="";
         for(int i=0;i<strs[0].length();i++){
             for(int j=1;j<strs.length;j++){
-                if(i>=strs[j].length() || strs[j].charAt(i)!=strs[0].charAt(i)){
-                    return strs[0].substring(0,i);
+                if(i==strs[j].length() || strs[0].charAt(i)!=strs[j].charAt(i)){
+                    return str;
                 }
             }
+            str+=strs[0].charAt(i);
+
         }
-        return strs[0];
+        return str;
+        
+
     }
 }
