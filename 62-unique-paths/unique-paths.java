b806da3e065d362e928499f1cc0eb975735f76ad
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-07-31 00:07:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (13.10%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -6,23 +6,20 @@ class Solution {
         if(i<0 || j<0){
             return 0;
         }
-
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-
         int up=func(i-1, j, dp);
         int left=func(i, j-1, dp);
 
         return dp[i][j]=up+left;
     }
     public int uniquePaths(int m, int n) {
-        int i=m-1;
-        int j=n-1;
         int[][] dp=new int[m][n];
         for(int[] row:dp){
             Arrays.fill(row, -1);
         }
-        return func(i, j, dp);   
+        return func(m-1, n-1, dp);
+           
     }
 }
