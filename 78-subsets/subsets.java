242380e136c969822522cf979484ddaa4004d946
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-07-31 00:45:25 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (75.57%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,20 +1,23 @@
 class Solution {
-    private void printSubsets(int[] nums, int index, List<List<Integer>> list, List<Integer> subList){
+    private void print(int index, List<Integer> subList, List<List<Integer>> list, int[] nums){
         if(index>=nums.length){
             list.add(new ArrayList<>(subList));
             return;
         }
+
         subList.add(nums[index]);
-        printSubsets(nums, index+1, list, subList);
+        print(index+1, subList, list, nums);
         subList.remove(subList.size()-1);
-        printSubsets(nums, index+1, list, subList);
-        
+        print(index+1, subList, list, nums);
+
     }
+   
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list=new ArrayList<>();
         List<Integer> subList=new ArrayList<>();
         int index=0;
-        printSubsets(nums, index, list, subList);
+        print(index, subList, list, nums);
+
         return list;
         
     }
