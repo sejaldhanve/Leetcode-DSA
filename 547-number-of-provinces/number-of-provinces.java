e4889708c8b3e53341205632f839f9c4d4a3ef6f
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-07-30 18:00:13 +0530

    Time: 2 ms (41.80%) | Memory: 49.9 MB (6.87%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,35 +1,34 @@
 class Solution {
-    private void dfs(int node, ArrayList<ArrayList<Integer>> adjLs, int[] visited){
-        visited[node]=1;
-        for(Integer it:adjLs.get(node)){
-            if(visited[it]==0){
-                dfs(it, adjLs, visited);
+    private void dfs(int node, int[][] isConnected, ArrayList<ArrayList<Integer>> adjList, int[] vis ){
+        vis[node]=1;
+        for(int it:adjList.get(node)){
+            if(vis[it]==0){
+                dfs(it, isConnected, adjList, vis);
             }
         }
-
     }
     public int findCircleNum(int[][] isConnected) {
         int count=0;
-        ArrayList<ArrayList<Integer>> adjLs=new ArrayList<>();
-        for(int i=0;i<isConnected.length;i++){
-            adjLs.add(new ArrayList<Integer>());
+        ArrayList<ArrayList<Integer>> adjList=new ArrayList<>();
+        for(int i=0;i<=isConnected.length;i++){
+            adjList.add(new ArrayList<>());
         }
+
         for(int i=0;i<isConnected.length;i++){
             for(int j=0;j<isConnected.length;j++){
                 if(isConnected[i][j]==1 && i!=j){
-                    adjLs.get(i).add(j);
-                    adjLs.get(j).add(i);
-
+                    adjList.get(i).add(j);
                 }
             }
         }
-        int[] visited=new int[adjLs.size()];
-        for(int i=0;i<visited.length;i++){
-            if(visited[i]==0){
+
+        int[] vis=new int[adjList.size()];
+        for(int i=0;i<vis.length;i++){
+            if(vis[i]==0){
                 count++;
-                dfs(i, adjLs, visited);
+                dfs(i, isConnected, adjList, vis);
             }
         }
-    return count;   
+        return count-1;
     }
 }
