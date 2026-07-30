731f032568147b8a2c6b47658e3dfe726f577d04
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-07-30 23:18:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (73.23%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,18 +1,14 @@
 class Solution {
-    private int func(int index, int[] dp){
-        if(index==0 || index==1){
-            return 1;
-        }
-
-        if(dp[index]!=-1){
-            return dp[index];
-        }
-
-        return dp[index]=func(index-1, dp)+func(index-2, dp);
-    }
     public int climbStairs(int n) {
         int[] dp=new int[n+1];
-        Arrays.fill(dp, -1);
-        return func(n, dp);    
+
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
     }
 }
