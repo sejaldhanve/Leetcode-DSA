45323cc45201f0357783e02b56dd7b31d5cb15eb
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-07-31 00:57:04 +0530

    Time: 2 ms (96.39%) | Memory: 45.5 MB (92.38%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,25 +1,25 @@
 class Solution {
-    private void printSub(int index, int[] candidates, int target, int sum, List<List<Integer>> list, List<Integer> subList){
-        if(index>=candidates.length){
-            if(sum==target){
-                list.add(new ArrayList<>(subList));
-            }
+    private void print(int i, List<List<Integer>> list, List<Integer> subList, int[] candidates, int target){
+        if(target==0){
+            list.add(new ArrayList<>(subList));
             return;
         }
-        if(sum+candidates[index]<=target){
-            subList.add(candidates[index]);
-            printSub(index, candidates, target, sum+candidates[index], list, subList);
+        if(i==candidates.length){
+            return;
+        }
+        if(target>=candidates[i]){
+            subList.add(candidates[i]);
+            print(i, list, subList, candidates, target-candidates[i]);
             subList.remove(subList.size()-1);
         }
-        printSub(index+1, candidates, target, sum, list, subList);
+        print(i+1, list, subList, candidates, target);
+
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        int index=0;
-        int sum=0;
         List<List<Integer>> list=new ArrayList<>();
-        List<Integer> subList=new ArrayList();
-
-        printSub(index, candidates, target, sum, list, subList);
+        List<Integer> subList=new ArrayList<>();
+        print(0, list, subList, candidates, target);
         return list;
+
     }
 }
