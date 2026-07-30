a2963623595769cc3596fd7d6773cfe2e3d83c4c
Sejal Dhanve <sejaldhanve1805@gmail.com>
2026-07-30 18:20:36 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (11.12%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,26 +1,24 @@
 class Solution {
-    private void dfs(int[][] ans, int[][] image, int row, int col, int iniColor, int newColor, int[] deltaRow, int[] deltaCol){
-        ans[row][col]=newColor;
+    private void dfs(int[][] image, int sr, int sc, int color, int[][] ans, int iniColor, int[] delRow, int[] delCol){
+        ans[sr][sc]=color;
         int n=image.length;
         int m=image[0].length;
         for(int i=0;i<4;i++){
-            int nrow=row+deltaRow[i];
-            int ncol=col+deltaCol[i];
+            int nrow=sr+delRow[i];
+            int ncol=sc+delCol[i];
 
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==iniColor && ans[nrow][ncol]!=newColor){
-                dfs(ans, image, nrow, ncol, iniColor, newColor, deltaRow, deltaCol);
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==iniColor && ans[nrow][ncol]!=color){
+                dfs(image, nrow, ncol, color, ans, iniColor, delRow, delCol);
             }
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int[][] ans=image;
         int iniColor=image[sr][sc];
-        int[] deltaRow={-1, 0, 1, 0};
-        int[] deltaCol={0, 1, 0, -1};
-        dfs(ans, image, sr, sc, iniColor, color, deltaRow, deltaCol);
-
+        int[] delRow={-1, 0, 1, 0};
+        int[] delCol={0, 1, 0, -1};
+        dfs(image, sr, sc, color, ans, iniColor, delRow, delCol);
         return ans;
-
-        
+   
     }
 }
